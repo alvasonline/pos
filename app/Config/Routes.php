5ce77418ee31b228afddce5394b03a3d07513922
Alexander Vasquez <alexv08@gmail.com>
2022-03-21 14:23:25 -0500

    Tratando de Colocar los css y js

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,7 @@ $routes->setAutoRoute(false);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->group('Front', ['namespace' => 'App\Controllers\Front'], function ($routes) {
-    $routes->get('', 'Home::index',['as' => 'home']);
+    $routes->get('/', 'Home::index',['as' => 'home']);
     $routes->get('unidades', 'Home::unidades',['as' => 'unidades']);
 });
 
